795d092a7323a4994f84fcd36e4b805731bd6f5a
qinxiaodong <qinsfu@outlook.com>
2023-04-05 14:12:37 +0800

    添加 UnitResult 表示空的执行结果

--- src/StudyOnline.Common/Model/Result.cs
+++ src/StudyOnline.Common/Model/Result.cs
@@ -1,6 +1,13 @@
 namespace Study402Online.Common.Model;
 
 /// <summary>
+/// 空的执行结果
+/// </summary>
+/// <param name="Error"></param>
+/// <param name="Success"></param>
+public record struct UnitResult(object Error, bool Success);
+
+/// <summary>
 /// 用来表示操作执行结果
 /// </summary>
 /// <typeparam name="TResult"></typeparam>
@@ -11,7 +18,32 @@ public record struct Result<TResult>(TResult Value, object Error, bool Success);
 /// </summary>
 public static class ResultFactory
 {
+    /// <summary>
+    /// 创建成功的执行结果
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="value"></param>
+    /// <returns></returns>
     public static Result<T> Success<T>(T value) => new Result<T>(value, default, true);
 
+    /// <summary>
+    /// 创建失败的执行结果
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="error"></param>
+    /// <returns></returns>
     public static Result<T> Fail<T>(object error) => new Result<T>(default, error, true);
+
+    /// <summary>
+    /// 创建空的成功执行结果
+    /// </summary>
+    /// <returns></returns>
+    public static UnitResult Success() => new UnitResult(default, false);
+
+    /// <summary>
+    /// 创建空的失败的执行结果
+    /// </summary>
+    /// <param name="error"></param>
+    /// <returns></returns>
+    public static UnitResult Fail(object error) => new UnitResult(error, true);
 }
