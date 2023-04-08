60e77b9c08ab852e97e99a82901c1f0230cf3268
qinxiaodong <qinsfu@outlook.com>
2023-04-08 14:50:23 +0800

    修改发布课程操作

--- src/Services/ContentService/ContentService.Api/Application/Commands/PublishCourseCommandHandler.cs
+++ src/Services/ContentService/ContentService.Api/Application/Commands/PublishCourseCommandHandler.cs
@@ -2,11 +2,13 @@ using Aliyun.OSS;
 using AutoMapper;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 using Microsoft.Extensions.Options;
 using RazorLight;
 using StackExchange.Redis;
 using Study402Online.Common.Model;
 using Study402Online.ContentService.Api.Application.Configurations;
+using Study402Online.ContentService.Api.Application.Services;
 using Study402Online.ContentService.Api.Infrastructure;
 using Study402Online.ContentService.Model.DataModel;
 using System.Security.Cryptography;
@@ -22,21 +24,25 @@ public class PublishCourseCommandHandler : IRequestHandler<PublishCourseCommand,
 {
     private readonly ContentDbContext _context;
     private readonly IMapper _mapper;
-    private readonly RazorLightEngine _razorEngine;
-    private readonly OssClient _oss;
-    private readonly IOptions<OssOptions> _options;
-    private readonly ConnectionMultiplexer _redis;
+    private readonly IMediaService _mediaService;
+    private readonly ISearchService _searchService;
+    private readonly ICacheService _cacheService;
+    private readonly ITemplateService _templateService;
 
     public PublishCourseCommandHandler(
-        ContentDbContext context, IMapper mapper, RazorLightEngine razorEngine,
-        OssClient oss, IOptions<OssOptions> options, ConnectionMultiplexer redis)
+        ContentDbContext context,
+        IMapper mapper,
+        ITemplateService templateService,
+        ICacheService cacheService,
+        IMediaService mediaService,
+        ISearchService searchService)
     {
         _context = context;
         _mapper = mapper;
-        _razorEngine = razorEngine;
-        _oss = oss;
-        _options = options;
-        _redis = redis;
+        _mediaService = mediaService;
+        _searchService = searchService;
+        _cacheService = cacheService;
+        _templateService = templateService;
     }
 
     public async Task<UnitResult> Handle(PublishCourseCommand request, CancellationToken cancellationToken)
@@ -51,25 +57,24 @@ public class PublishCourseCommandHandler : IRequestHandler<PublishCourseCommand,
 
         // TODO 这样的包含远程请求的事务，最好修改成为 SAGE ，可能
 
-        // TODO 移动记录到发布表
+        // 移动记录到发布表
         var course = await _context.Courses.SingleAsync(c => c.Id == request.Course);
 
         var publish = _mapper.Map<CoursePublish>(publishpre);
         await _context.CoursePublishes.AddAsync(publish);
 
-        // TODO 生成预览页面并存储到OSS
-        var previewPage = await _razorEngine.CompileRenderStringAsync("Course", "Course.rz", course);
-        using var previewPageStream = new MemoryStream(Encoding.UTF8.GetBytes(previewPage));
-        var previewPageHash = await SHA256.HashDataAsync(previewPageStream);
-        var previewPageObjectId = $"page/{previewPageHash[0..2]}/{previewPageHash[2..4]}/{previewPageHash}";
+        // 生成预览页面并存储到OSS
+        var previewPageResult = await _templateService.RenderAsync("Course", course);
+        if (previewPageResult is { Success: true })
+            _ = await _mediaService.UploadFileAsync($"/preview/course/{request.Course}", previewPageResult.Value);
+        previewPageResult.Value.Close();
 
-        _ = _oss.PutObject(_options.Value.FileBucket, previewPageObjectId, previewPageStream);
+        // 缓存课程信息到 Redis
+        await _cacheService.CacheRowAsync(course);
 
-        // TODO 缓存课程信息到 Redis
-        var db = _redis.GetDatabase();
-        await db.StringSetAsync($"course:{course.Id}", JsonSerializer.Serialize(course));
+        // 将课程存储到 ES
+        await _searchService.AddCourseDocumentAsync(course);
 
-        // TODO 将课程存储到 ES
 
         return ResultFactory.Success();
     }
