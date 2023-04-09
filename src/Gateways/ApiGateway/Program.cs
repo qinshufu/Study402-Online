9bc425fe013af16c5de1ef95576205d3900b3fba
qinxiaodong <qinsfu@outlook.com>
2023-04-09 17:24:43 +0800

    <ApiGateway> Ocelot 集成 Consul 做集中配置以及服务发现

--- src/Gateways/ApiGateway/Program.cs
+++ src/Gateways/ApiGateway/Program.cs
@@ -1,9 +1,10 @@
 using Ocelot.Middleware;
 using Ocelot.DependencyInjection;
+using Ocelot.Provider.Consul;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOcelot();
+builder.Services.AddOcelot().AddConsul().AddConfigStoredInConsul();
 
 builder.Configuration.AddJsonFile("ocelot.json");
 
